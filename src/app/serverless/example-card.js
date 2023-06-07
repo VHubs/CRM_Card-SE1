0e13b82dbf0757c237a37b30c3e54008db6a1e84
VHubs <129496376+VHubs@users.noreply.github.com>
2023-06-07 15:33:28 +0200

    Update example-card.js

--- src/app/serverless/example-card.js
+++ src/app/serverless/example-card.js
@@ -1,11 +1,11 @@
-// For external API calls
+/ For external API calls
 const axios = require("axios");
 
 exports.main = async (context = {}, sendResponse) => {
   // Store contact firstname, configured as propertiesToSend in crm-card.json
   const { firstname } = context.propertiesToSend;
 
- const introMessage = 
+  const introMessage = 
     {
     "type": "tile",
     "content": [
@@ -42,45 +42,88 @@ exports.main = async (context = {}, sendResponse) => {
   }
     ]
 };
-  
-  const KPIs = 
-    {
-    "type": "tile",
-    "content": [
-        {
-            "type": "heading",
-            "text": "Data"
-        },
-        {
-            "type": "statistics",
-            "items": [
-                {
-                    "label": "Views last week",
-                    "number": "55",
-                    "description": "Apr 11 - Apr 17"
-                },
-                {
-                    "label": "Views this month",
-                    "number": "203",
-                    "description": {
-                        "type": "trend",
-                        "value": "23.36%",
-                        "direction": "increase"
-                    }
-                },
-                {
-                    "label": "Markdown syntax",
-                    "number": "405",
-                    "description": {
-                        "type": "text",
-                        "format": "markdown",
-                        "text": "[Feb 12 - Feb 19](https://app.hubspot.com/)"
-                    }
-                }
-            ]
-        }
-    ]
-};
     
+  const nextSteps = [
+    {
+      type: "alert",
+      title: "Alert: something you should be aware of",
+      variant: "error",
+      body: [{
+        type: "text",
+        text: "click on the following url to either view or redirected to the document"
+      },
+    ]
+    },
+    {
+      type: "tile",
+      content: 
+        {
+          type: "heading",
+          text: "Medical Data"
+        },
+      },
+
+    {
+      type: "divider",
+      distance: "small",
+    },
+    {
+      type: "heading",
+      text: "Next Steps",
+    },
+    {
+      type: "text",
+      format: "markdown",
+      text: "You can explore more [code samples](https://github.com/HubSpot/ui-extensions-examples) or UI components in the [CRM card builder](https://app.hubspot.com/l/extensible-ui/), or try code samples. If you get stuck, take look at your [build or deploy logs](https://app.hubspot.com/l/developer-projects/) or [serverless functions logs](https://app.hubspot.com/l/private-apps/).",
+    },
+  ];
+
+  try {
+    const { data } = await axios.get("https://zenquotes.io/api/random");
+
+    const quoteSections = [
+      {
+        type: "tile",
+        body: [
+          {
+            type: "text",
+            format: "markdown",
+            text: `**Hello ${firstname}, here's your quote for the day**!`,
+          },
+          {
+            type: "text",
+            format: "markdown",
+            text: `_${data[0].q}_`,
+          },
+          {
+            type: "text",
+            format: "markdown",
+            text: `_**Author**: ${data[0].a}_`,
+          },
+        ],
+      },
+      {
+        type: "button",
+        text: "Get new quote",
+        onClick: {
+          type: "SERVERLESS_ACTION_HOOK",
+          serverlessFunction: "crm-card",
+        },
+      },
+    ];
+
+    sendResponse({
+      sections: [introMessage],
+    });
+  } catch (error) {
+    // "message" will create an error feedback banner when it catches an error
+    sendResponse({
+      message: {
+        type: "ERROR",
+        body: `Error: ${error.message}`,
+      },
+      sections: [introMessage],
+    });
+  }
 };
 
