35c52b72cfe3be393a13da45f5e6f54b4aaf25c4
VHubs <129496376+VHubs@users.noreply.github.com>
2023-06-07 14:28:55 +0200

    Update example-card.js

--- src/app/serverless/example-card.js
+++ src/app/serverless/example-card.js
@@ -15,34 +15,6 @@ const introMessage =
 			},
     ]
     }; 
-const statistics = [{
-  "type": "statistics",
-  "items": [
-   {
-    "label": "Views last week",
-    "number": "55",
-    "description": "Apr 11 - Apr 17"
-   },
-   {
-    "label": "Views this month",
-    "number": "203",
-    "description": {
-      "type": "trend",
-      "value": "23.36%",
-      "direction": "increase"
-      }
-    },
-    {
-      "label": "Markdown syntax",
-      "number": "405",
-      "description": {
-         "type": "text",
-         "format": "markdown",
-         "text": "[Feb 12 - Feb 19](https://app.hubspot.com/)"
-     }
-   }
-  ]
-	];
   
   const nextSteps = [
     {
@@ -103,6 +75,34 @@ const statistics = [{
           },
         ],
       },
+	    {
+  "type": "statistics",
+  "items": [
+   {
+    "label": "Views last week",
+    "number": "55",
+    "description": "Apr 11 - Apr 17"
+   },
+   {
+    "label": "Views this month",
+    "number": "203",
+    "description": {
+      "type": "trend",
+      "value": "23.36%",
+      "direction": "increase"
+      }
+    },
+    {
+      "label": "Markdown syntax",
+      "number": "405",
+      "description": {
+         "type": "text",
+         "format": "markdown",
+         "text": "[Feb 12 - Feb 19](https://app.hubspot.com/)"
+     }
+   }
+  ]
+},
       {
         type: "button",
         text: "Get new quote",
