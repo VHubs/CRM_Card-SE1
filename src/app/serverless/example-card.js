8ff27dcd3f1184a71e1a4b6ab2b3b0ed4ecf47fb
VHubs <129496376+VHubs@users.noreply.github.com>
2023-06-07 14:54:51 +0200

    Update example-card.js

--- src/app/serverless/example-card.js
+++ src/app/serverless/example-card.js
@@ -11,10 +11,38 @@ const introMessage =
 		"content": [
 			{
 				"type": "heading",
-				"text": "Medical Data"
+				"text": "Data"
 			},
-    ]
-    };
+    			]
+    },
+      {
+  "type": "statistics",
+  "items": [
+   {
+    "label": "Views last week",
+    "number": "55",
+    "description": "Apr 11 - Apr 17"
+   },
+   {
+    "label": "Views this month",
+    "number": "203",
+    "description": {
+      "type": "trend",
+      "value": "23.36%",
+      "direction": "increase"
+      }
+    },
+    {
+      "label": "Markdown syntax",
+      "number": "405",
+      "description": {
+         "type": "text",
+         "format": "markdown",
+         "text": "[Feb 12 - Feb 19](https://app.hubspot.com/)"
+     }
+   }
+  ]
+};
     
   const nextSteps = [
     {
