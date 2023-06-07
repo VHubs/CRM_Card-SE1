6df96cf335cf0b823dce997425446d741355bb6a
VHubs <129496376+VHubs@users.noreply.github.com>
2023-06-07 13:37:17 +0200

    Update example-card.js

--- src/app/serverless/example-card.js
+++ src/app/serverless/example-card.js
@@ -8,12 +8,10 @@ exports.main = async (context = {}, sendResponse) => {
 	
   const tileOne = {
 "type": "crm-card",
-"location":"crm.record.tab",
 "content": [
 {
 "type": "heading",
 "text": "Custom Card Example",
-
 },
 {
 "type": "alert",
@@ -69,51 +67,4 @@ exports.main = async (context = {}, sendResponse) => {
 ]
 };
 
-  try {
-    const { data } = await axios.get("https://zenquotes.io/api/random");
-
-    const quoteSections = [
-      {
-        type: "tile",
-        body: [
-          {
-            type: "text",
-            format: "markdown",
-            text: `**Hello ${firstname}, here's your quote for the day**!`,
-          },
-          {
-            type: "text",
-            format: "markdown",
-            text: `_${data[0].q}_`,
-          },
-          {
-            type: "text",
-            format: "markdown",
-            text: `_**Author**: ${data[0].a}_`,
-          },
-        ],
-      },
-      {
-        type: "button",
-        text: "Get new quote",
-        onClick: {
-          type: "SERVERLESS_ACTION_HOOK",
-          serverlessFunction: "crm-card",
-        },
-      },
-    ];
-
-    sendResponse({
-      sections: [tileOne],
-    });
-  } catch (error) {
-    // "message" will create an error feedback banner when it catches an error
-    sendResponse({
-      message: {
-        type: "ERROR",
-        body: `Error: ${error.message}`,
-      },
-      sections: [tileOne],
-    });
-  }
-};
+  
