c9b90336d344599ce451cf3e717939756daa5b76
VHubs <129496376+VHubs@users.noreply.github.com>
2023-06-07 13:54:03 +0200

    Update example-card.js

--- src/app/serverless/example-card.js
+++ src/app/serverless/example-card.js
@@ -5,16 +5,15 @@ exports.main = async (context = {}, sendResponse) => {
   // Store contact firstname, configured as propertiesToSend in crm-card.json
   const { firstname } = context.propertiesToSend;
 
-  const tileOne = {
-"type": "crm-card",
-"location":"crm.record.tab",
-"content": [
-{
-"type": "heading",
-"text": "Custom Card Example",
-
-},
-{
+const tileOne = 
+    {
+		"type": "tile",
+		"content": [
+			{
+				"type": "heading",
+				"text": "CRM Card"
+			},
+      {
 "type": "alert",
 "title": "Alert: something you should be aware of",
 "variant": "error",
@@ -22,50 +21,91 @@ exports.main = async (context = {}, sendResponse) => {
 "type": "text",
 "text": "click on the following url to either view or redirected to the document"
 }
-},
-{
-"type": "divider",
-"distance": "small"
-},
-{
-"type": "descriptionList",
-"items": [
-{
-"label": "Name:",
-"value": "John Smith"
-},
-{
-"label": "Address:",
-"value": "Am Postbahnhof 17, Berlin, Germany"
-},
-{
-"label": "Schedule:",
-"value": {
-"type": "text",
-"format": "markdown",
-"text": "[link to schedule](https://app.hubspot.com/l/docs/doc/platform/create-custom-crm-cards-with-projects#components)"
-}
-}
-]
-},
-{
-"type": "tag",
-"text": "Waiting for validation",
-"variant": "warning",
-"onClick": {
-"type": "SERVERLESS_ACTION_HOOK",
-"serverlessFunction": "exampleFunction"
-}
-},
-{
-"type": "button",
-"text": "Contact",
-"onClick": {
-"type": "SERVERLESS_ACTION_HOOK",
-"serverlessFunction": "exampleFunction"
-}
-}
-]
+    ]
+    };
+    
+  const nextSteps = [
+    {
+      type: "alert",
+      title: "Alert: something you should be aware of",
+      variant: "error",
+      body: [{
+        type: "text",
+        text: "click on the following url to either view or redirected to the document"
+      },
+    ]
+    },
+    {
+      type: "tile",
+      content: 
+        {
+          type: "heading",
+          text: "Medical Data"
+        },
+      },
+
+    {
+      type: "divider",
+      distance: "small",
+    },
+    {
+      type: "heading",
+      text: "Next Steps",
+    },
+    {
+      type: "text",
+      format: "markdown",
+      text: "You can explore more [code samples](https://github.com/HubSpot/ui-extensions-examples) or UI components in the [CRM card builder](https://app.hubspot.com/l/extensible-ui/), or try code samples. If you get stuck, take look at your [build or deploy logs](https://app.hubspot.com/l/developer-projects/) or [serverless functions logs](https://app.hubspot.com/l/private-apps/).",
+    },
+  ];
+
+  try {
+    const { data } = await axios.get("https://zenquotes.io/api/random");
+
+    const quoteSections = [
+      {
+        type: "tile",
+        body: [
+          {
+            type: "text",
+            format: "markdown",
+            text: `**Hello ${firstname}, here's your quote for the day**!`,
+          },
+          {
+            type: "text",
+            format: "markdown",
+            text: `_${data[0].q}_`,
+          },
+          {
+            type: "text",
+            format: "markdown",
+            text: `_**Author**: ${data[0].a}_`,
+          },
+        ],
+      },
+      {
+        type: "button",
+        text: "Get new quote",
+        onClick: {
+          type: "SERVERLESS_ACTION_HOOK",
+          serverlessFunction: "crm-card",
+        },
+      },
+    ];
+
+    sendResponse({
+      sections: [introMessage, ...quoteSections, ...nextSteps],
+    });
+  } catch (error) {
+    // "message" will create an error feedback banner when it catches an error
+    sendResponse({
+      message: {
+        type: "ERROR",
+        body: `Error: ${error.message}`,
+      },
+      sections: [introMessage],
+    });
+  }
 };
   try {
     const { data } = await axios.get("https://zenquotes.io/api/random");
