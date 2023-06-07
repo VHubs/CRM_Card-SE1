ed1d9eada4998d71a7fee1575e3449d9c3cac388
VHubs <129496376+VHubs@users.noreply.github.com>
2023-06-07 13:50:50 +0200

    Update example-card.js

--- src/app/serverless/example-card.js
+++ src/app/serverless/example-card.js
@@ -5,15 +5,16 @@ exports.main = async (context = {}, sendResponse) => {
   // Store contact firstname, configured as propertiesToSend in crm-card.json
   const { firstname } = context.propertiesToSend;
 
-  const introMessage = 
-    {
-		"type": "tile",
-		"content": [
-			{
-				"type": "heading",
-				"text": "Custom Card"
-			},
-			{
+  const tileOne = {
+"type": "crm-card",
+"location":"crm.record.tab",
+"content": [
+{
+"type": "heading",
+"text": "Custom Card Example",
+
+},
+{
 "type": "alert",
 "title": "Alert: something you should be aware of",
 "variant": "error",
@@ -21,44 +22,51 @@ exports.main = async (context = {}, sendResponse) => {
 "type": "text",
 "text": "click on the following url to either view or redirected to the document"
 }
-    
-    };
-    
-  const nextSteps = [
-    {
-      type: "alert",
-      title: "Alert: something you should be aware of",
-      variant: "error",
-      body: [{
-        type: "text",
-        text: "click on the following url to either view or redirected to the document"
-      },
-    ]
-    },
-    {
-      type: "tile",
-      content: 
-        {
-          type: "heading",
-          text: "Medical Data"
-        },
-      },
-
-    {
-      type: "divider",
-      distance: "small",
-    },
-    {
-      type: "heading",
-      text: "Next Steps",
-    },
-    {
-      type: "text",
-      format: "markdown",
-      text: "You can explore more [code samples](https://github.com/HubSpot/ui-extensions-examples) or UI components in the [CRM card builder](https://app.hubspot.com/l/extensible-ui/), or try code samples. If you get stuck, take look at your [build or deploy logs](https://app.hubspot.com/l/developer-projects/) or [serverless functions logs](https://app.hubspot.com/l/private-apps/).",
-    },
-  ];
-
+},
+{
+"type": "divider",
+"distance": "small"
+},
+{
+"type": "descriptionList",
+"items": [
+{
+"label": "Name:",
+"value": "John Smith"
+},
+{
+"label": "Address:",
+"value": "Am Postbahnhof 17, Berlin, Germany"
+},
+{
+"label": "Schedule:",
+"value": {
+"type": "text",
+"format": "markdown",
+"text": "[link to schedule](https://app.hubspot.com/l/docs/doc/platform/create-custom-crm-cards-with-projects#components)"
+}
+}
+]
+},
+{
+"type": "tag",
+"text": "Waiting for validation",
+"variant": "warning",
+"onClick": {
+"type": "SERVERLESS_ACTION_HOOK",
+"serverlessFunction": "exampleFunction"
+}
+},
+{
+"type": "button",
+"text": "Contact",
+"onClick": {
+"type": "SERVERLESS_ACTION_HOOK",
+"serverlessFunction": "exampleFunction"
+}
+}
+]
+};
   try {
     const { data } = await axios.get("https://zenquotes.io/api/random");
 
