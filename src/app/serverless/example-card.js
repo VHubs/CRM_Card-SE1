0bcff73466100357baa555835f7ad9fa61506e0e
VHubs <129496376+VHubs@users.noreply.github.com>
2023-06-07 13:35:33 +0200

    Update example-card.js

--- src/app/serverless/example-card.js
+++ src/app/serverless/example-card.js
@@ -7,68 +7,67 @@ exports.main = async (context = {}, sendResponse) => {
 	
 	
   const tileOne = {
-    "type": "tile",
-    "content": [
-      {
-        "type": "heading",
-        "text": "Custom Card"      
-      },
-	    ]
-  };
-	
+"type": "crm-card",
+"location":"crm.record.tab",
+"content": [
 {
-				"type": "alert",
-				"title": "Alert: something you should be aware of",
-				"variant": "error",
-				"body": {
-					"type": "text",
-					"text": "click on the following url to either view or redirected to the document"
-				}
-			},
-			{
-				"type": "divider",
-				"distance": "small"
-			},
-			{
-				"type": "descriptionList",
-				"items": [
-					{
-						"label": "Name:",
-						"value": "John Smith"
-					},
-					{
-						"label": "Address:",
-						"value": "Am Postbahnhof 17, Berlin, Germany"
-					},
-					{
-						"label": "Schedule:",
-						"value": {
-							"type": "text",
-							"format": "markdown",
-							"text": "[link to schedule](https://app.hubspot.com/l/docs/doc/platform/create-custom-crm-cards-with-projects#components)"
-						}
-					}
-				]
-			},
-			{
-				"type": "tag",
-				"text": "Waiting for validation",
-				"variant": "warning",
-				"onClick": {
-					"type": "SERVERLESS_ACTION_HOOK",
-					"serverlessFunction": "exampleFunction"
-				}
-			},
-			{
-				"type": "button",
-				"text": "Contact",
-				"onClick": {
-					"type": "SERVERLESS_ACTION_HOOK",
-					"serverlessFunction": "exampleFunction"
-				}
-			}
-    ]
-  };
+"type": "heading",
+"text": "Custom Card Example",
+
+},
+{
+"type": "alert",
+"title": "Alert: something you should be aware of",
+"variant": "error",
+"body": {
+"type": "text",
+"text": "click on the following url to either view or redirected to the document"
+}
+},
+{
+"type": "divider",
+"distance": "small"
+},
+{
+"type": "descriptionList",
+"items": [
+{
+"label": "Name:",
+"value": "John Smith"
+},
+{
+"label": "Address:",
+"value": "Am Postbahnhof 17, Berlin, Germany"
+},
+{
+"label": "Schedule:",
+"value": {
+"type": "text",
+"format": "markdown",
+"text": "[link to schedule](https://app.hubspot.com/l/docs/doc/platform/create-custom-crm-cards-with-projects#components)"
+}
+}
+]
+},
+{
+"type": "tag",
+"text": "Waiting for validation",
+"variant": "warning",
+"onClick": {
+"type": "SERVERLESS_ACTION_HOOK",
+"serverlessFunction": "exampleFunction"
+}
+},
+{
+"type": "button",
+"text": "Contact",
+"onClick": {
+"type": "SERVERLESS_ACTION_HOOK",
+"serverlessFunction": "exampleFunction"
+}
+}
+]
+};
 
   try {
     const { data } = await axios.get("https://zenquotes.io/api/random");
