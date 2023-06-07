d26b2c60611bea393b6f235e109aaf98661df1de
VHubs <129496376+VHubs@users.noreply.github.com>
2023-06-07 12:59:54 +0200

    Update example-card.js

--- src/app/serverless/example-card.js
+++ src/app/serverless/example-card.js
@@ -4,32 +4,10 @@ const axios = require("axios");
 exports.main = async (context = {}, sendResponse) => {
   // Store contact firstname, configured as propertiesToSend in crm-card.json
   const { firstname } = context.propertiesToSend;
-
-	const tileOne = {
-  "type": "crm-card",
-  "data": {
-    "title": "Example CRM Card",
-    "location": "crm.record.tab",
-    "fetch": {
-      "targetFunction": "crm-card",
-      "objectTypes": [
-        {
-          "name": "contacts",
-          "propertiesToSend": ["firstname"]
-        }
-      ]
-    }
-  }
-};
-	
-	
-	
-	
 	
 	
   const tileOne = {
     "type": "crm-card",
-"location":"crm.record.tab",
     "content": [
       {
         "type": "heading",
