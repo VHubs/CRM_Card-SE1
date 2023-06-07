2ff769048b62994e14608da1c06fa11b7399c5d2
VHubs <129496376+VHubs@users.noreply.github.com>
2023-06-07 16:34:45 +0200

    Update example-card.js

--- src/app/serverless/example-card.js
+++ src/app/serverless/example-card.js
@@ -132,7 +132,7 @@ const iframeTile = {
         // Width and height of the iframe (in pixels)
         "width": 700,
         "height": 400,
-        "uri": 'https://www.youtube.com/embed/FDumsLFwyNM',
+        "uri": 'https://files.coinmarketcap.com/static/widget/currency.js',
       },
     },
   ],
